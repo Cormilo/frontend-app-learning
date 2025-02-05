70bcafc1cae83035ea48128cb2f87c2f65a3c90a
Cormilo <gitgitgithub@yandex.ru>
2025-02-05 16:46:12 +0300

    всегда открыты курсы

--- src/course-home/outline-tab/OutlineTab.test.jsx
+++ src/course-home/outline-tab/OutlineTab.test.jsx
@@ -150,7 +150,7 @@ describe('Outline Tab', () => {
 
       // Section initially renders collapsed
       const collapsedSectionNode = screen.getByRole('button', { name: /section/ });
-      expect(collapsedSectionNode).toHaveAttribute('aria-expanded', 'false');
+      expect(collapsedSectionNode).toHaveAttribute('aria-expanded', 'true');
 
       // Click to expand section
       userEvent.click(expandButton);
@@ -158,7 +158,7 @@ describe('Outline Tab', () => {
 
       // Click to collapse section
       userEvent.click(expandButton);
-      await waitFor(() => expect(collapsedSectionNode).toHaveAttribute('aria-expanded', 'false'));
+      await waitFor(() => expect(collapsedSectionNode).toHaveAttribute('aria-expanded', 'true'));
     });
 
     it('displays correct icon for complete assignment', async () => {
