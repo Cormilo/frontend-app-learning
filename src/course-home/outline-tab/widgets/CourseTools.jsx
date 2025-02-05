8de4f5e74a00e3ba24a3fb292bca82b4f897a753
Cormilo <gitgitgithub@yandex.ru>
2025-02-05 17:41:30 +0300

    del courses tools

--- src/course-home/outline-tab/widgets/CourseTools.jsx
+++ src/course-home/outline-tab/widgets/CourseTools.jsx
@@ -62,22 +62,7 @@ const CourseTools = ({ intl }) => {
   };
 
   return (
-    <section className="mb-4">
-      <h2 className="h4">{intl.formatMessage(messages.tools)}</h2>
-      <ul className="list-unstyled">
-        {courseTools.map((courseTool) => (
-          <li key={courseTool.analyticsId} className="small">
-            <a href={courseTool.url} onClick={() => logClick(courseTool.analyticsId)}>
-              <FontAwesomeIcon icon={renderIcon(courseTool.analyticsId)} className="mr-2" fixedWidth />
-              {courseTool.title}
-            </a>
-          </li>
-        ))}
-        <li className="small" id="courseHome-launchTourLink">
-          <LaunchCourseHomeTourButton />
-        </li>
-      </ul>
-    </section>
+    ' '
   );
 };
 
