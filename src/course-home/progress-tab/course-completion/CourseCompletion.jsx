742248e80d1912306afffa23ab393638b776f616
Cormilo <gitgitgithub@yandex.ru>
2025-02-05 22:24:16 +0300

    trans Couses complited

--- src/course-home/progress-tab/course-completion/CourseCompletion.jsx
+++ src/course-home/progress-tab/course-completion/CourseCompletion.jsx
@@ -8,9 +8,9 @@ const CourseCompletion = ({ intl }) => (
   <section className="text-dark-700 mb-4 rounded raised-card p-4">
     <div className="row w-100 m-0">
       <div className="col-12 col-sm-6 col-md-7 p-0">
-        <h2>{intl.formatMessage(messages.courseCompletion)}</h2>
+        <h2>Course completion</h2>
         <p className="small">
-          {intl.formatMessage(messages.completionBody)}
+        This represents how much of the course content you have completed. Note that some content may not yet be released.
         </p>
       </div>
       <div className="col-12 col-sm-6 col-md-5 mt-sm-n3 p-0 text-center">
