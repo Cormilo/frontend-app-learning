06dd66f725ce9a1ab9f009597cff8056077347a2
Cormilo <gitgitgithub@yandex.ru>
2025-02-09 22:34:55 +0300

    change body size in dashboard 1

--- src/tab-page/LoadedTabPage.jsx
+++ src/tab-page/LoadedTabPage.jsx
@@ -69,7 +69,7 @@ const LoadedTabPage = ({
         streakDiscountCouponEnabled={streakDiscountCouponEnabled}
         verifiedMode={verifiedMode}
       />
-      <main id="main-content" className="d-flex flex-column flex-grow-1">
+      <main id="main-content" className="d-flex flex-column flex-grow-1" style={{ minHeight: '85%' }}>
         <AlertList
           topic="outline"
           className="mx-5 mt-3"
